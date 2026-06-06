a40278faf63d07add45e4761580a4e89882a71e3
odinaka <odinakanda0@gmail.com>
2026-06-05 23:30:35 -0400

    bin and packages directory now

--- lela/main.py
+++ lela/main.py
@@ -1,22 +1,32 @@
 import os
-from datetime import datetime
 import json
+import subprocess
+from datetime import datetime
 
 def create_env(env_name=""):
+    #TODO: Custom env names and directories
+    """
     if env_name == "":
         env_name = '.venv'
+
+    """
+
+    #TODO: Allow users choose their version of python
+
     curr_dir = os.getcwd()
     venv_dir = os.path.join(curr_dir, r'.venv')
     if not os.path.exists(venv_dir):
         os.makedirs(venv_dir)
         package_dir = os.path.join(venv_dir, r'packages')
         os.makedirs(package_dir)
+        bin_dir = os.path.join(venv_dir, r'bin')
+        os.makedirs(bin_dir)
         now = datetime.now()
         curr_time = now.strftime("%Y-%m-%d %H:%M:%S")
         time_data = {
             'created_at': curr_time
         }
-        with open(".venv/meta.json", "w", encoding="utf-8") as file:
+        with open(".venv/metadata.json", "w", encoding="utf-8") as file:
             json.dump(time_data, file, indent=4)
 
     else:
@@ -24,6 +34,15 @@ def create_env(env_name=""):
         return 0
     return 1
 
+
+def activate_env():
+    pass
+
+def deactivate_env():
+    pass
+
+def delete_env():
+    pass
 
 def main():
     create_env()
