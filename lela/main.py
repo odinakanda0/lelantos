d2b671ded3a82601989a254e2fdfd8abbb2ec10e
odinaka <odinakanda0@gmail.com>
2026-06-12 19:45:20 -0400

    got symlink workiing

--- lela/main.py
+++ lela/main.py
@@ -1,33 +1,38 @@
 import os
+import sys
 import json
 import subprocess
 from datetime import datetime
 
 def create_env(env_name=""):
-    #TODO: Custom env names and directories
-    """
     if env_name == "":
         env_name = '.venv'
-
-    """
-
-    #TODO: Allow users choose their version of python
-
     curr_dir = os.getcwd()
-    venv_dir = os.path.join(curr_dir, r'.venv')
+    venv_dir = os.path.join(curr_dir, rf'{env_name}')
     if not os.path.exists(venv_dir):
         os.makedirs(venv_dir)
         package_dir = os.path.join(venv_dir, r'packages')
         os.makedirs(package_dir)
         bin_dir = os.path.join(venv_dir, r'bin')
         os.makedirs(bin_dir)
-        now = datetime.now()
-        curr_time = now.strftime("%Y-%m-%d %H:%M:%S")
-        time_data = {
-            'created_at': curr_time
+        python_version=sys.version.split(' ')[0]
+        python_path_components=sys.executable.split('/')
+        python_home="/".join(python_path_components[:-1])
+        python_executable="/".join(python_path_components)
+        version_data = {
+            "python_version": python_version
         }
-        with open(".venv/metadata.json", "w", encoding="utf-8") as file:
-            json.dump(time_data, file, indent=4)
+        create_symlink = subprocess.run(["ln", "-s", f"{python_executable}", f"{bin_dir}"], capture_output=True, text=True)
+        print(create_symlink.stdout.strip())
+        
+        with open(os.path.join(venv_dir, "pyvenv.cfg"), "w") as file:
+            file.write(f"home = {python_home}\n")
+            file.write(f"version = {python_version}\n")
+            file.write(f"executable = {python_executable}\n")
+            file.write("include-system-site-packages = false\n")
+
+        with open(os.path.join(venv_dir, "metadata.json"), "w", encoding="utf-8") as file:
+            json.dump(version_data, file, indent=4)
 
     else:
         print("A virtual environment (.venv) already exists")
